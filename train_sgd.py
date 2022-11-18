07bf6494cf56316dd189b0e3d5f06ee4b7fc9957
Ryan Slater <32707042+rslater-cs@users.noreply.github.com>
2022-11-18 14:55:56 +0000

    changed classifier head and accuracy

--- train_sgd.py
+++ train_sgd.py
@@ -1,38 +1,61 @@
+import torch
 from torch import cuda
 import torch.optim as optim
-from torch.nn import CrossEntropyLoss, Softmax
-from torch.utils.data import DataLoader
+from torch.nn import CrossEntropyLoss, Softmax, Sequential, Dropout, Linear
+from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import CIFAR10
-from torchvision.transforms import ToTensor
-from torchvision.models import resnet18, ResNet18_Weights
+from torchvision.models import efficientnet_b1, EfficientNet_B1_Weights
+from torchvision import transforms
 from torch import argmax
+from torchvision.transforms import ToTensor
 
 from tqdm import tqdm
 # import ssl
 
 # ssl._create_default_https_context = ssl._create_unverified_context
 
-EPOCHS = 20
-BATCH_SIZE = 512
+EPOCHS = 100
+BATCH_SIZE = 16
 
 device = "cuda:0" if cuda.is_available() else "cpu"
 
 print("Using", device)
 
-model = resnet18(weights=ResNet18_Weights.DEFAULT)
+classifier = Sequential(
+    Dropout(0.2),
+    Linear(1280, 10, bias=True),
+)
+
+model = efficientnet_b1()
+model.classifier = classifier
 model = model.to(device)
+model.train()
 print(model)
 
 criterion = CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=1e-5)
+optimizer = optim.SGD(model.parameters(), lr=1e-3)
 
-transform = ToTensor()
+transform = transforms.Compose([
+    transforms.Resize(240),
+    ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+])
+
+val_len = 5000
 trainset = CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_len = len(trainset)-val_len
+trainset, valset = random_split(trainset, [train_len, val_len])
+
 testset = CIFAR10(root='./data', train=False, download=True, transform=transform)
+test_len = len(testset)
+
 train_loader = DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(valset, batch_size=BATCH_SIZE, shuffle=False)
 test_loader = DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False)
 
 softmax = Softmax(1)
+
+max_val_acc = 0.0
 
 for epoch in range(EPOCHS):
     tr_accuracy = 0.0
@@ -56,16 +79,36 @@ for epoch in range(EPOCHS):
             tr_accuracy += (sft_outputs == labels).float().sum()
             progress += 1
 
-            tepoch.set_postfix({"loss":loss.item(), "accuracy":(100*((tr_accuracy.item())/(progress*BATCH_SIZE)))})
+            tepoch.set_postfix({"loss":loss.item(), "accuracy":(100*((tr_accuracy.item())/train_len))})
     
     val_accuracy = 0.0
-    for inputs, labels in iter(test_loader):
+    for inputs, labels in iter(val_loader):
         inputs, labels = inputs.to(device), labels.to(device)
 
         outputs = model(inputs)
         
         max_outputs = argmax(softmax(outputs), dim=1)
         val_accuracy += (max_outputs == labels).float().sum()
+    
+    val_accuracy = (val_accuracy/val_len).item()*100
+    print('\t\tval_accuracy: {0}'.format(val_accuracy))
 
-    print('\t\tval_accuracy: {0}'.format((val_accuracy/len(test_loader)).item()*100))
+    if(val_accuracy > max_val_acc):
+        max_val_acc = val_accuracy
+        torch.save(model.state_dict(), "./models/best_effnet_sgd.pth")
+        print("\t\tSaved state")
+
+test_accuracy = 0.0
+for inputs, labels in iter(test_loader):
+    inputs, labels = inputs.to(device), labels.to(device)
+
+    outputs = model(inputs)
+    
+    max_outputs = argmax(softmax(outputs), dim=1)
+    test_accuracy += (max_outputs == labels).float().sum()
+    
+test_accuracy = (test_accuracy/test_len).item()*100
+
+print("Test Accuracy: {0}".format(test_accuracy))
+torch.save(model.state_dict(), "./models/final_effnet_sgd.pth")
 
